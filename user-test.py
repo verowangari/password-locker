deff7d7471495308e5d41a97a8e8695161812245
verowangari <verowangari34@gmail.com>
2022-02-19 16:35:25 +0300

    create a test case that checks if user object is saved in the user list

--- user-test.py
+++ user-test.py
@@ -11,4 +11,16 @@ def setUp(self):
     setUp: method that runs before each testcases.
     '''
     self.new_user=User("Veronica","Rodriguez","2022")
+def test_init(self):
+        '''
+        test_init: test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.user_name,"Veronica")
+        self.asserEqual(self.new_user.password,"2022")
+def test_save_user(self):
+    '''
+    test_save_user: test case to test if the user object is saved into the user list
+    '''
+    self.new_user.save_user()#save new contact
+    self.asserEqual(len(User.user_list),1)
     
