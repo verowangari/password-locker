e960b79b7d83ece96123047f23925cd9ae762a89
verowangari <verowangari34@gmail.com>
2022-02-19 16:12:52 +0300

    create contact object

--- user-test.py
+++ user-test.py
@@ -6,3 +6,9 @@ class Testuser(unittest.TestCase):
     Test class that defines test cases for user class behaviors.
     unittest.TestCase: Testcase class that helps in creating test cases
     '''
+def setUp(self):
+    '''
+    setUp: method that runs before each testcases.
+    '''
+    self.new_user=User("Veronica","Rodriguez","2022")
+    
